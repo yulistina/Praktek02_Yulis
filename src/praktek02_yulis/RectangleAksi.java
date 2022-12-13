969703696aa45b1efaf31c0648edb23b75af9133
yulistina <yulistina101@gmail.com>
2022-12-14 11:30:28 +1400

    Mengisi Nilai Atribut

--- src/praktek02_yulis/RectangleAksi.java
+++ src/praktek02_yulis/RectangleAksi.java
@@ -12,5 +12,8 @@ package praktek02_yulis;
 public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
+        r1.panjang = 9;
+        r1.lebar= 3;
+        
     }
 }
