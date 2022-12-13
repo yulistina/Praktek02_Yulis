36c60eaeb4cca514e028febedc077259ac7f6372
yulistina <yulistina101@gmail.com>
2022-12-14 11:54:20 +1400

    contructor - sama dengan nama class

--- src/praktek02_yulis/Rectangle.java
+++ src/praktek02_yulis/Rectangle.java
@@ -12,11 +12,27 @@ package praktek02_yulis;
 public class Rectangle {
     double panjang ;
     double lebar;
+
+    public Rectangle() {
+        panjang=5;
+        lebar=2; 
+    }
+    
+    
     
     void cetakinfo(){
         System.out.println("====================");
         System.out.println("panjang : "+panjang);
         System.out.println("lebar: "+lebar);
         System.out.println("====================");    
+           
+    }
+         double hitungLuas(){
+         double luas;
+         luas = panjang * lebar;
+         return luas;
+    }
+         void cetakLuas(){
+         System.out.println("Luasnya adalah:"+hitungLuas()); 
     }
 }
